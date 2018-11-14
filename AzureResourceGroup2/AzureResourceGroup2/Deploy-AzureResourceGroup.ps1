ae9870f751c2773842605b878ff1425995ea9db2
Daniel-Mihai David <daviddanielm@outlook.com>
2018-11-14 13:11:29 +0200

    replace template

--- AzureResourceGroup2/AzureResourceGroup2/Deploy-AzureResourceGroup.ps1
+++ AzureResourceGroup2/AzureResourceGroup2/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'ExchangeDeploy',
+    [string] $ResourceGroupName = 'AzureResourceGroup2',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
